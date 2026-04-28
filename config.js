0b46ee2c199950bac163e593ac657a912bf25949
hyptryx <gharry1979@gmail.com>
2026-04-28 15:29:19 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBLMm-ECInporSOM88PVhf567g1LC_T_Y",
+  authDomain: "hrx-support-tracker.firebaseapp.com",
+  projectId: "hrx-support-tracker",
+  storageBucket: "hrx-support-tracker.firebasestorage.app",
+  messagingSenderId: "769147887711",
+  appId: "1:769147887711:web:7b45c975070f15d7627bea"
+};
